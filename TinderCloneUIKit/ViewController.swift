368318f0c370418ce2f55ce6a048d78dda6d151c
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-05-11 21:33:09 +0300

    Initial Commit

---
+++ TinderCloneUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderCloneUIKit
+//
+//  Created by YILDIRIM on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
